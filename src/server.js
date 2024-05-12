47cf3d474c394cb79e1509f19f8f28ecbd530295
Edvo1901 <DongVo1901@gmail.com>
2024-05-13 02:57:21 +0930

    create database syntax

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 const connection = require("./config/database");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
@@ -10,17 +11,26 @@ const port = process.env.PORT || 8080;
 const hostname = process.env.HOST_NAME;
 
 // Config request body
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 configViewEngine(app);
 
 // Initializing routes
 app.use("/", webRoutes);
 
-// Test db connection
-connection();
-
-app.listen(port, hostname, () => {
-	console.log(`Example app listening on port ${port}`);
+const kittySchema = new mongoose.Schema({
+	name: String,
 });
+
+const Kitten = mongoose.model("Kitten", kittySchema);
+const cat = new Kitten({ name: "Maybe" });
+cat.save();
+
+// Test db connection
+(async () => {
+	await connection();
+	app.listen(port, hostname, () => {
+		console.log(`Example app listening on port ${port}`);
+	});
+})();
