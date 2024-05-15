73855f5077e542e07e2220d452aeddc0e8925a1a
Edvo1901 <DongVo1901@gmail.com>
2024-05-16 08:06:43 +0930

    add task to project

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,11 +1,28 @@
 const mongoose = require("mongoose");
 const aqp = require("api-query-params");
 const Task = require("../models/task");
+const Project = require("../models/project");
 
 const postCreateTaskService = async (data) => {
 	try {
 		if (data.type === "EMPTY-TASK") {
 			const result = await Task.create(data);
+			return result;
+		} else if (data.type === "ADD-TASKS") {
+			let myProject = await Project.findById(data.projectId).exec();
+
+			for (let i = 0; i < data.tasksArr.length; i++) {
+                const taskItem = data.tasksArr[i]
+
+                if (mongoose.Types.ObjectId.isValid(taskItem)) {
+					myProject.tasks.push(mongoose.Types.ObjectId(taskItem));
+				} else {
+					throw new Error(`Invalid ObjectId: ${taskItem}`);
+				}
+			}
+            
+            const result = await myProject.save();
+
 			return result;
 		}
 	} catch (err) {
@@ -33,10 +50,7 @@ const getAllTaskService = async (queryData) => {
 
 const putUpdateTaskService = async (data) => {
 	try {
-		const result = await Task.updateOne(
-			{ _id },
-			{ ...data}
-		);
+		const result = await Task.updateOne({ _id }, { ...data });
 		return result;
 	} catch (err) {
 		console.log(err);
